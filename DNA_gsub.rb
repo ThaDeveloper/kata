e091e14e7461ef15d2cb30ea9844f8a18f2a0737
Justin Ndwiga <jndwiga@SamaDc.corp>
2017-03-09 10:15:58 +0300

    7kyu

--- DNA_gsub.rb
+++ DNA_gsub.rb
@@ -1,3 +1,8 @@
 def DNA_strand(dna)
    dna.gsub(/[ATGC]/, 'A' => 'T', 'T' => 'A','G' => 'C', 'C' => 'G')
 end
+=begin
+def DNA_strand(dna)
+  dna.tr("ACTG", "TGAC")
+end
+=end
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Kataa kata, kata usiogope
+
